7be9cc3bdbcb72b362ccdf088e8a2089ad90156b
lakshmi-udaan <lakshmi.rk@udaan.com>
2021-05-11 19:39:51 +0530

    test if reviewdog comments on delta changes

--- src/ch01/1.1_ATasteOfKotlin.kt
+++ src/ch01/1.1_ATasteOfKotlin.kt
@@ -3,8 +3,8 @@ package ch01.ex1_ATasteOfKotlin
 data class Person(val name: String,val age: Int? = null)
 
 fun main(args: Array<String>) {
-    val persons = listOf(Person("Alice"),
-                         Person("Bob", age = 29))
+    val persons = listOf(Person("Bob"),
+                         Person("Alice", age = 29))
 
     val oldest = persons.maxBy { it.age ?: 0 }
     println("The oldest is: $oldest")
