fc5c15aef3e1e01d49c59d668e6bc90b3851c346
lakshmi-udaan <lakshmi.rk@udaan.com>
2022-01-08 00:31:23 +0530

    testing final.1.1

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd doc/docs/sections/ || exit
-cat ./badges.md ./title.md ./contents.md ./about.md ./getting-started.md ./integrate-snorql-to-project.md \
+cat ./badges.md ../index.md ./contents.md ./about.md ./getting-started.md ./integrate-snorql-to-project.md \
 ./enable-snorql-extensions.md ./build-custom-metrics.md ./kotlin-documentation.md ./roadmap.md \
 ../../../CONTRIBUTING.md ./license-details.md ./contacts.md ./acknowledgements.md > ../../../result.md
